c6d843918a564d3dae66274d2210d42416224668
silasfacanha <silasfacanha@hotmail.com>
2022-07-29 14:35:22 -0300

    I created the random quote generator

--- pages/routes/random.js
+++ pages/routes/random.js
@@ -1,6 +1,12 @@
+const quotesJSONArray = require("./quotesSource.json");
+function getRandom(size) {
+  return Math.floor(Math.random() * size);
+}
+
 function random(request, response) {
-  const random = new Date();
-  response.json({ date: random });
+  const randomIndex = getRandom(quotesJSONArray.length);
+  const randomQuote = quotesJSONArray[randomIndex];
+  response.json({ quote: randomQuote });
 }
 
 export default random;
