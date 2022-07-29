2b77a20ca55518a98721a6395fc49bd188ed997e
silasfacanha <silasfacanha@hotmail.com>
2022-07-29 14:49:08 -0300

    Changing the random.js file

--- pages/routes/random.js
+++ pages/routes/random.js
@@ -2,10 +2,10 @@ const quotesJSONArray = require("./../../quotesSource.json");
 function getRandom(size) {
   return Math.floor(Math.random() * size);
 }
+const randomIndex = getRandom(quotesJSONArray.length);
+const randomQuote = quotesJSONArray[randomIndex];
 
 function random(request, response) {
-  const randomIndex = getRandom(quotesJSONArray.length);
-  const randomQuote = quotesJSONArray[randomIndex];
   response.json({ quote: randomQuote });
 }
 
